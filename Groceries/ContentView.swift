321204f9cf431f7114060d4a92cef43c983e32f0
GuilhermeDeconto <deconto100@gmail.com>
2020-01-14 14:21:02 -0300

    Create Project

---
+++ Groceries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Groceries
+//
+//  Created by Guilherme Deconto on 14/01/20.
+//  Copyright © 2020 Bullseye Groceries. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
