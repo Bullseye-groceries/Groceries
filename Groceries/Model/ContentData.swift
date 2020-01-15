ac86f438135aecf8079f9d85906e52faf8f29d7e
Marco Mangan <marcomangan73@gmail.com>
2020-01-15 14:55:17 -0300

    Adding new images, adding new data.

--- Groceries/Model/ContentData.swift
+++ Groceries/Model/ContentData.swift
@@ -13,18 +13,19 @@ class ContentData: ObservableObject {
         [
             Product(description: "Molho de Tomate Quero Tradicional 340g", image: "tomato", ean: "71231268312361",
                     payments: [
-                Payment(merchant: "Carrefour", price: 2.99, units: 2, paidAt: Date.init()),
-                Payment(merchant: "Zaffari da Cristovao", price: 3.55, units: 3, paidAt: Date.init()),
+                        Payment(merchant: "Carrefour", price: 2.99, units: 2, paidAt: Date.init()),
+                        Payment(merchant: "Zaffari da Cristóvão", price: 3.55, units: 3, paidAt: Date.init()),
             ], suggestedPrice: 2.99),
-            Product(description: "Cebola-roxa kg", image: "tomato", ean: "7896202900759", payments: [
+            Product(description: "Cebola-roxa kg", image: "cebola", ean: "7896202900759", payments: [
                 Payment(merchant: "Zaffari da Cabral", price: 2.95, units: 0.8, paidAt: Date.init()),
-                    
+                
             ], suggestedPrice: 2.95),
-            Product(description: "Leite de coco Ducoco 200 ml", image: "tomato", ean: "71231268312361hsj", payments: [
-                Payment(merchant: "Zaffari da Cabral", price: 3.75, units: 1, paidAt: Date.init()),
+            Product(description: "Coco seco kg", image: "coco", ean: "7898318200129", payments: [
+                Payment(merchant: "Zaffari da Cabral", price: 5.98, units: 1, paidAt: Date.init()),
                 
             ], suggestedPrice: 3.75),
-            Product(description: "Molho de Tomate Quero Tradicional 1kg", image: "tomato", ean: "71231268312361hsj", payments: [], suggestedPrice: 2.99),
+            Product(description: "Limão Taiti kg" , image: "limao", ean: "7898945024419", payments: [
+                Payment(merchant: "Zaffari da Cabral", price: 5.98, units: 1, paidAt: Date.init()),], suggestedPrice: 5.98),
     ]
     
 }
