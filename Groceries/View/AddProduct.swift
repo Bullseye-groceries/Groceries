bc6e58912d8e5c06c0a5dfe794999d39281311d8
Gabriel Fanto <gabrielfanto@10-41-48-12.0200.epoa.br.iosda.org>
2020-01-15 15:25:00 -0300

    first commit on add product

--- Groceries/View/AddProduct.swift
+++ Groceries/View/AddProduct.swift
@@ -20,14 +20,26 @@ struct AddProduct: View {
             //TODO: FIXME: Image(product.image).resizable().frame(width: 100, height: 100, alignment: .center)
             
             TextField("Descrição", text: $product.description)
-            TextField("EAN", text: $product.ean)
+                .frame(width: 300, height: 60, alignment: .center)
+                .foregroundColor(.gray)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Código do Produto", text: $product.ean)
+                .frame(width: 300, height: 60, alignment: .center)
+                .foregroundColor(.gray)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
             Spacer()
             Button(action: {
                 print("Adding product...")
                 self.listProducts.append(self.product)
             }) {
                 Text("Cadastrar produto")
-            }
+            }.frame(width: 150, height: 50, alignment: .center)
+                .foregroundColor(.black)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.yellow
+                ]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(30)
             Spacer()
             }
         }
