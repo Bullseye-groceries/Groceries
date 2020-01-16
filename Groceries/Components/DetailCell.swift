056760d962bea0a01a0aab696299bb4d0c72a70a
Giuseppe Menti <mentifg@gmail.com>
2020-01-16 15:26:11 -0300

    Ajustes no detalhe

--- Groceries/Components/DetailCell.swift
+++ Groceries/Components/DetailCell.swift
@@ -15,6 +15,7 @@ struct DetailCell: View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .circular).fill(Color.white).shadow(radius: 2)
             HStack {
+                Spacer()
                 VStack {
                     Text(self.payment.merchant)
                     Text(DateFormatter.localizedString(from: self.payment.paidAt, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.none))
@@ -22,9 +23,10 @@ struct DetailCell: View {
                 Spacer()
                 VStack {
                     Text("R$ " + String(format: "%.2f", self.payment.price) + " x " + String(self.payment.units))
-                    Text("R$ " + String(format: "%.2f", self.payment.price * self.payment.units))
+                    Text("R$ " + String(format: "%.2f", self.payment.price * self.payment.units)).bold()
                 }.frame(alignment: .trailing)
-            }.padding(10)
+                Spacer()
+            }
         }
     }
 }
